080841c0395d4ef0698d70d165042fcc2638aa6c
haideralbaba-rgb <haideralbaba130@gmail.com>
2026-08-21 10:05:05 +0300

    Add Hero component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
+import Hero from "@/components/Hero";
 
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#070707] text-[#f5efe5]">
+      <Hero />
+    </main>
+  );
+}
